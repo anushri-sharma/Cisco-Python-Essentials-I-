9cc19f7fcf09230d4b2a59a0956da47123738cf4
Anushri Sharma <anushrisharma01@gmail.com>
2026-05-20 17:51:41 +0530

    Update Leap Year.py

--- Leap Year.py
+++ Leap Year.py
@@ -1,3 +1,5 @@
+# Essentials of the if-elif-else statement
+
 # Scenario
 # As you surely know, due to some astronomical reasons, years may be leap or common. 
 The former are 366 days long, while the latter are 365 days long.
