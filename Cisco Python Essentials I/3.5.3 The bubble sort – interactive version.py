64b8930e3f6d0e13522d3c064ed44e76dd146588
Anushri Sharma <anushrisharma01@gmail.com>
2026-05-22 20:31:53 +0530

    Update 3.5.3 The bubble sort – interactive version.py

--- Cisco Python Essentials I/3.5.3 The bubble sort – interactive version.py
+++ Cisco Python Essentials I/3.5.3 The bubble sort – interactive version.py
@@ -17,3 +17,25 @@ while swapped:
 
 print("\nSorted:")
 print(my_list)
+
+```
+Output
+How many elements do you want to sort: 3
+Enter a list element: 42
+Enter a list element: 21
+Enter a list element: 98
+
+Sorted:
+[21.0, 42.0, 98.0]
+```
+
+> Python, however, has its own sorting mechanisms.
+
+> If you want Python to sort your list, you can do it like this:
+
+my_list = [8, 10, 6, 2, 4]
+my_list.sort()
+print(my_list)
+
+
+
